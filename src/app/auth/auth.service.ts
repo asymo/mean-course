7d2ec11b82e611d1ed7df58c564a26fc310ee0c1
Christopher J Symons <cjsymons86@gmail.com>
2019-03-18 15:47:13 +0000

    Added authentication service and interceptor

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,8 +4,13 @@ import { AuthData } from './auth-data.model';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
+  private userToken;
 
   constructor(private http: HttpClient) {}
+
+  getToken() {
+    return this.userToken;
+  }
 
   createUser(email: string, password: string) {
     const authUser: AuthData = {
@@ -15,6 +20,17 @@ export class AuthService {
     this.http.post('http://localhost:3000/api/user/signup', authUser)
       .subscribe(response => {
         console.log(response);
+      });
+  }
+
+  login(email: string, password: string) {
+    const authUser: AuthData = {
+      email: email,
+      password: password
+    };
+    this.http.post<{ token: string }>('http://localhost:3000/api/user/login', authUser)
+      .subscribe(response => {
+        this.userToken = response.token;
       });
   }
 
